77b223df6d0337ede18500b5af2422b0e07e94b2
Hannu Huhtanen <hannu.huhtanen@gmail.com>
2019-04-26 13:23:40 +0300

    Added working integration with the API

--- assets/scripts/comments.js
+++ assets/scripts/comments.js
@@ -2,6 +2,8 @@
 
 const e = React.createElement;
 
+const API_URL = 'https://9xpxcvmaaa.execute-api.eu-west-1.amazonaws.com/dev/posts/1/comments';
+
 class Comment extends React.Component {
 
   render() {
@@ -26,9 +28,9 @@ class Comment extends React.Component {
 class CommentList extends React.Component {
 
   render() {
-    const listOfComments = this.props.comments.map((comment) =>
+    const listOfComments = this.props.comments.map((comment, index) =>
       e(
-        Comment, {author: comment.author, timestamp: comment.timestamp.toDateString(), content: comment.content}
+        Comment, {key: index, author: comment.author, timestamp: comment.timestamp.toDateString(), content: comment.content}
       )
     );
     return e(
@@ -39,6 +41,110 @@ class CommentList extends React.Component {
 
 }
 
+class NewCommentControls extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {commentContent: '', commentAuthor: 'Anonymous'};
+
+    this.onNewComment = this.props.onNewComment;
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    fetch(API_URL, {
+      headers: { "Content-Type": "application/json; charset=utf-8" },
+      method: 'POST',
+      body: JSON.stringify({
+        comment: this.state.commentContent,
+        author: this.state.commentAuthor
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        //alert(data.message + " " + JSON.stringify(data.comment));
+        this.state.commentContent = "";
+        this.onNewComment(parseApiComment(data.comment));
+      });
+  }
+
+  render() {
+    return e(
+      'div', {className: 'comment-add-controls'},
+      e(
+        'form', {onSubmit: this.handleSubmit},
+        e(
+          'textarea', {id: 'newCommentContent', name: 'commentContent', value: this.state.commentContent, onChange: this.handleChange}
+        ),
+        e(
+          'input', {type: 'text', id: 'newCommentAuthor', name: 'commentAuthor', value: this.state.commentAuthor, onChange: this.handleChange}
+        ),
+        e(
+          'input', {type: 'submit', id: 'newCommentSubmitButton'}
+        )
+      )
+    )
+  }
+}
+
+class CommentModule extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {comments: []};
+
+    this.onNewComment = this.onNewComment.bind(this);
+
+    this.updateList();
+
+  }
+
+  updateList() {
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => {
+        const parsedComments = data.comments.map(parseApiComment);
+        this.setState({comments: parsedComments});
+      });
+  }
+
+  onNewComment(newComment) {
+    this.updateList();
+  }
+
+  render() {
+    return e(
+      'div', {className: 'comment-module'},
+      e('h2', null, 'Comments'),
+      e(CommentList, {comments: this.state.comments}),
+      e(NewCommentControls, {onNewComment: this.onNewComment})
+    )
+  }
+
+}
+
+function parseApiComment(comment) {
+  return {
+    author: comment.comment_author,
+    content: comment.comment_content,
+    timestamp: new Date(comment.comment_timestamp)
+  }
+}
+
 const comments = [
   {author: 'Bob', timestamp: new Date(), content: 'First!'},
   {author: 'Alice', timestamp: new Date(), content: 'Repost. Unsubscribe!'},
@@ -46,4 +152,4 @@ const comments = [
 ];
 
 const domContainer = document.querySelector('#goldfish_thread');
-ReactDOM.render(e(CommentList, {comments: comments}), domContainer);
+ReactDOM.render(e(CommentModule), domContainer);
